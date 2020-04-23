d1755e8e3346e9103b414339d4d050c9da0f4ac4
yash chaudhary <yash240196@gmail.com>
2020-04-23 11:52:11 +0530

    curry solution

---
+++ js-exercises/curry/curry.js
@@ -0,0 +1,12 @@
+const curry = (fn) => {
+  const curryN = (n, fn) => (...args) =>
+    args.length >= n
+      ? fn(...args)
+      : curryN(n - args.length, (...innerArgs) => fn(...args, ...innerArgs))
+
+  return curryN(fn.length, fn)
+}
+
+export { curry }
+
+export { curry }
